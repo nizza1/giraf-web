e526c853239222dd546cfa0391bac1cbf612fc44
nizar <nizar@nizars-Air.fritz.box>
2023-08-28 23:05:44 +0200

    navigations

--- app/components/header/header.jsx
+++ app/components/header/header.jsx
@@ -1,6 +1,6 @@
+"use client"
 
-
-import React from 'react'
+import React, {useState} from 'react'
 import Logo from 'next/image'
 
 //import styles 
@@ -10,15 +10,31 @@ import Link from 'next/link'
 import Titlesmall from '@/app/components/title/titlesmall/title'
 
 
+
 const Header = () => {
+  const [isOpen , setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <header className={styles.outContainer}>
 
         <div className={styles.innerContainer}>
+
         <div className={styles.logoContainer}>
             <Titlesmall text='GIRAF'/>
         </div>
+         
+        
+                <button 
+                onClick={toggleMenu}
+                className={`${styles.navButton} ${isOpen ? styles.open : ''}`}><div className={styles.line}></div>
+                </button>
+           
 
+       
       
             <div className={styles.navmenu}>
             <Link className={styles.links} href="/">Home</Link>
