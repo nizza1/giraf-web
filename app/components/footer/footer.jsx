a2fd80b0f755a694c801799d7e47425a5b23d7b3
nizar <nizar@nizars-MacBook-Air.local>
2023-08-30 15:51:59 +0200

    footer logo nuance

--- app/components/footer/footer.jsx
+++ app/components/footer/footer.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Link from 'next/link'
 
+import Image from 'next/image'
+
 import styles from './styels.module.css'
 const Footer = () => {
   return (
@@ -25,6 +27,23 @@ const Footer = () => {
                href="/dataprotection" >
                 Privacy Policy & Imprint
               </Link>
+            </div>
+
+            <div className={styles.nuance}>
+            <p>Created by Nuance Studio</p>
+            <div className={styles.linkContainer}>
+            <a href="https://www.nuancestudio.de/" target="_blank"        rel="noopener noreferrer">
+              <Image
+              className={styles.logo}
+               src="nuance.svg"
+                alt="nuance studio"
+                width={100}
+                 height={50}
+                 />
+             </a>
+
+            </div>
+            
             </div>
     
         </div>
