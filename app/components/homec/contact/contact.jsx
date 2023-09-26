0705a284f248036edd8d4310183a782380658152
nizar <nizar@nizars-MacBook-Air.local>
2023-09-26 14:33:26 +0200

    contact form and validations

--- app/components/homec/contact/contact.jsx
+++ app/components/homec/contact/contact.jsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React from 'react'
+import React , {useState} from 'react'
 
 
 import styles from './styles.module.css'
@@ -15,6 +15,48 @@ import contactAnimations from '@/app/animations/animation_ll5965nn.json';
 
 /* container to form changed */
 const Contact = () => {
+
+  const [formData , setFormData] = useState({
+    email : '',
+    phone : '',
+    name : '',
+    message : '',
+  })
+
+  const handleChange = (e) => {
+    const {name , value} = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    })
+  }
+
+  const handleSubmit = async(e) => {
+    e.preventDefault()
+    try {
+      const response = await fetch('/api/contact', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          accept: 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await response.json();
+      if (data.success) {
+
+        alert(data.message);
+      } else {
+       
+        alert(data.message);
+      }
+    } catch (error) {
+      alert('Error:'+ error);
+    }
+  }
+
+
+
   return (
     <div className={styles.outContainer} id='contact-form'>
         <div className={styles.innerSection} >
@@ -31,7 +73,7 @@ const Contact = () => {
 
         </div>
 
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={handleSubmit}>
      
       <div className={`${styles.email} ${styles.block}`}>
         <label  htmlFor="frm-email">Email</label>
@@ -42,6 +84,8 @@ const Contact = () => {
           placeholder="e.g steve@example.com"
           autoComplete="email"
           required
+          value={formData.email} 
+          onChange={handleChange} 
         />
       </div>
       <div className={`${styles.phone} ${styles.block}`}>
@@ -53,6 +97,8 @@ const Contact = () => {
           name="phone"
           autoComplete="tel"
           required
+          value={formData.phone} 
+          onChange={handleChange} 
         />
       </div>
       <div className={`${styles.name} ${styles.block}`}>
@@ -62,9 +108,11 @@ const Contact = () => {
             id="frm-first"
             placeholder="e.g steve"
             type="text"
-            name="first"
+            name="name"
             autoComplete="given-name"
             required
+            value={formData.name} 
+            onChange={handleChange} 
           />
         </div>
    
@@ -72,6 +120,8 @@ const Contact = () => {
       <div className={`${styles.message} ${styles.block}`}>
         <label htmlFor="frm-message">Message</label>
         <textarea className={styles.textarea} id="frm-message" rows="6" name="message" placeholder="your message"
+        value={formData.message} 
+        onChange={handleChange} 
         ></textarea>
       </div>
       <div className={`${styles.button} ${styles.block}`}>
---
+++ pages/api/contact.js
@@ -0,0 +1,44 @@
+
+/* require('dotenv').config()
+const PASSWORD = process.env.password */
+
+import { mailOptions, transporter } from "/config/nodemailer";
+
+/* sollte SMTP stattdessen Port 587 nutzen  */
+
+  const handler = async(req, res) => {
+
+    if (req.method === 'POST'){
+      const data = req.body;
+
+      if (!data.name) {
+        return res.status(400).json({ message: 'Name is required' });
+      }
+      if (!data.email) {
+        return res.status(400).json({ message: 'Email is required' });
+      }
+      // Add more validation rules as needed
+  
+      try {
+        await transporter.sendMail({
+            ...mailOptions,
+            subject: 'my website',
+            text: 'this is a test string',
+            html: `<h3>E-Mail : ${data.email}</h3> 
+            <h3>phone : ${data.phone}</h3> 
+            <h3>Name : ${data.name}</h3>
+            <h3> message :<br>  <br> ${data.message}</h3>`
+          })
+  
+        return res.status(200).json({success:true , message: 'Your message has been sent' });
+      } catch (error) {
+        console.error(error);
+        return res.status(400).json({success:false , message: error.message});
+      }
+    }
+  
+    return res.status(405).json({ message: 'Method not allowed' });
+      
+    }
+  
+   export default handler
