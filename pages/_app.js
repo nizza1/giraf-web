a3dfdae349d16ecf63fa9481016b3bca17e86337
nizar <nizar@nizars-MacBook-Air.local>
2023-09-28 12:45:15 +0200

    h

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react'
 import Layout from '@/app/components/layout/layout'
 import '@/app/globals.css'
-import styles from './app.module.css'
+
 
 import { Inter } from 'next/font/google'
 
