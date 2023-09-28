1b4c62b7eaad7a708c6694659592c87775ce886e
nizar <nizar@nizars-MacBook-Air.local>
2023-09-28 11:41:54 +0200

    j

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -2,13 +2,13 @@ import nodemailer from 'nodemailer';
 
 
 const email = process.env.EMAIL;
-const pass= process.env.EMAIL_PASS;
+const AppPassword= process.env.EMAIL_PASS;
 
 export const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
         user: email,
-        pass:process.env.EMAIL_PASS
+        pass: AppPassword
     }
 });
 
